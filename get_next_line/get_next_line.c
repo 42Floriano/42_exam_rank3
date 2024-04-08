b6b3d83e6f75c2c25c39087d3766f4479f3dccb3
Floriano Albertini <56269861+IronWyatt@users.noreply.github.com>
2024-04-09 00:55:40 +0200

    gnl finished

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -3,14 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: falberti <falberti@student.42.fr>          +#+  +:+       +#+        */
+/*   By: albertini <albertini@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 17:31:07 by falberti          #+#    #+#             */
-/*   Updated: 2024/04/08 18:06:34 by falberti         ###   ########.fr       */
+/*   Updated: 2024/04/09 00:48:39 by albertini        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+//#include "get_next_line.h"
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define BUFFER_SIZE 42
 
 int	ft_strlen(char *str)
 {
@@ -23,27 +29,27 @@ int	ft_strlen(char *str)
 
 char	*ft_strchr(char c, char *str)
 {
-	int i = 0;
-
-	while (str[i] || str[i] != c)
+	while (*str)
 	{
-		
-		i++;
-	}	
+		if (*str == c)
+			return (str);
+		str++;
+	}
 	return (NULL);
 }
 
-//done
-void	*ft_strcopy(char *dst, char *src)
+static void	*ft_strcopy(char *dst, char *src)
 {
 	int i = 0;
 
 	while (src[i])
-		dst[i] = src[i++];
+	{
+		dst[i] = src[i];
+		i++;
+	}
 	dst[i] = '\0';
 }
 
-//done
 char	*ft_strdup(char *str)
 {
 	char *new;
@@ -55,6 +61,7 @@ char	*ft_strdup(char *str)
 	return (new);
 }
 
+//done
 char	*ft_strjoin(char *strOne, char *strTwo)
 {
 	char *new = malloc(sizeof(char) * (ft_strlen(strOne) + ft_strlen(strTwo)) + 1);
@@ -69,5 +76,59 @@ char	*ft_strjoin(char *strOne, char *strTwo)
 
 char *get_next_line(int fd)
 {
+	static char	buf[BUFFER_SIZE + 1];
+	char	*line;
+	char	*newline;
+	int		countread;
+	int		to_copy;
 
+	line = ft_strdup(buf);
+	while (!(newline = ft_strchr('\n', line))
+				&& (countread = read(fd, buf, BUFFER_SIZE)))
+	{
+		buf[countread] = '\0';
+		line = ft_strjoin(line, buf);
+	}
+	if (ft_strlen(line) == 0)
+	{
+		free(line);
+		return (NULL);
+	}
+	if (newline != NULL)
+	{
+		to_copy = newline - line + 1;
+		ft_strcopy(buf, newline + 1);
+	}
+	else
+	{
+		to_copy = ft_strlen(line);
+		buf[0] = '\0';
+	}
+	line[to_copy] = '\0';
+	return (line);
 }
+
+// int main(int argc, char **argv)
+// {
+//     int fd;
+//     char *line;
+
+//     if (argc != 2) {
+//         printf("Usage: %s <filename>\n", argv[0]);
+//         return 1;
+//     }
+
+//     fd = open(argv[1], O_RDONLY);
+//     if (fd == -1) {
+//         perror("Error opening file");
+//         return 1;
+//     }
+
+//     while ((line = get_next_line(fd)) != NULL) {
+//         printf("%s", line);
+//         free(line);
+//     }
+
+//     close(fd);
+//     return 0;
+// }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: falberti <falberti@student.42.fr>          +#+  +:+       +#+        */
+/*   By: albertini <albertini@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 17:31:04 by falberti          #+#    #+#             */
-/*   Updated: 2024/04/08 17:40:05 by falberti         ###   ########.fr       */
+/*   Updated: 2024/04/09 00:22:49 by albertini        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+
 
 #ifndef BUFFER_SIZE
 # define BUFFER_SIZE 42
