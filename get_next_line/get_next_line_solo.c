5d5e01174515d3064f855f40a5e95127379c0428
Floriano <floriano.webdev@gmail.com>
2024-04-09 11:40:26 +0200

    backup

--- get_next_line/get_next_line_solo.c
+++ get_next_line/get_next_line_solo.c
@@ -3,14 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_solo.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: albertini <albertini@student.42.fr>        +#+  +:+       +#+        */
+/*   By: falberti <falberti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 10:11:38 by albertini         #+#    #+#             */
-/*   Updated: 2024/04/09 10:40:19 by albertini        ###   ########.fr       */
+/*   Updated: 2024/04/09 11:39:28 by falberti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_solo.h"
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif 
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
 
 int	ft_strlen(char *str)
 {
@@ -32,17 +38,6 @@ char	*ft_strchr(char c, char *str)
 	return (NULL);
 }
 
-char	*ft_strdup(char *str)
-{
-	char *new = malloc(sizeof(char) * (ft_strlen(str) + 1));
-	int i = 0;
-
-	if (!new)
-		return (NULL);
-	ft_strcopy(new, str);
-	return (new);
-}
-
 void	ft_strcopy(char *dst, char *src)
 {
 	int i = 0;
@@ -54,6 +49,16 @@ void	ft_strcopy(char *dst, char *src)
 	}
 	dst[i] = '\0';
 	return ;
+}
+
+char	*ft_strdup(char *str)
+{
+	char *new = malloc(sizeof(char) * (ft_strlen(str) + 1));
+
+	if (!new)
+		return (NULL);
+	ft_strcopy(new, str);
+	return (new);
 }
 
 char	*ft_strjoin(char *strone, char *strtwo)
@@ -74,6 +79,7 @@ char	*ft_strjoin(char *strone, char *strtwo)
 		new[i++] = strtwo[y++];
 	}
 	new[i] = '\0';
+	free(strone);
 	return (new);
 }
 
@@ -82,11 +88,31 @@ char	*get_next_line(int fd)
 	static char buf[BUFFER_SIZE + 1];
 	char	*line;
 	char	*newline;
-	int		to_read = 0;
-	int		countreaded = 0;
+	int		to_copy = 0;
+	int		countread = 0;
 	
 	line = ft_strdup(buf);
-	while(!(newline = ft_strchr('\n', buf)) && )
-	while(!())
+	while (!(newline = ft_strchr('\n', line))
+			&& (countread = read(fd, buf, BUFFER_SIZE)))
+	{
+		buf[countread] = '\0';
+		line = ft_strjoin(line, buf);
+	}
+	if (ft_strlen(line) == 0)
+	{
+		free(line);
+		return (NULL);
+	}
+	if (newline != NULL)
+	{
+		to_copy = newline - line + 1;
+		ft_strcopy(buf, newline + 1);
+	}
+	else
+	{
+		to_copy = ft_strlen(line);
+		buf[0] = '\0';
+	}
+	line[to_copy] = '\0';
 	return (line);
 }
